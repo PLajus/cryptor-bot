bfd10a06d6e820acb855ac492fbfb03c56aaf813
Povilas Lajus <povilas.lajus@gmail.com>
2021-06-16 18:20:15 +0300

    Added 24hr symbol price change

--- src/cryptor.py
+++ src/cryptor.py
@@ -25,8 +25,8 @@ class Cryptor(commands.Cog):
         if self.binance.test_connectivity() is not None:
             await ctx.send("funds are safu")
 
-    # Post latest symbol price
-    @commands.command(help="Displays the latest price for a symbol.")
+    # Get latest symbol price
+    @commands.command(help="Displays the latest price of a symbol.")
     async def price(self, ctx, symbol):
         json_data = self.binance.get_price(symbol).json()
         if "code" in json_data:
@@ -36,6 +36,32 @@ class Cryptor(commands.Cog):
                 await ctx.send("Pair does not exist.")
         elif "price" in json_data:
             await ctx.send(f"{symbol}: {float(json_data['price'])}")
+
+    # Get the 24hr price change of a symbol
+    @commands.command(
+        help="Displays the 24hr. price change of a symbol.", aliases=["24change"]
+    )
+    async def change24(self, ctx, symbol):
+        json_data = self.binance.get_24hrdata(symbol).json()
+        if "code" in json_data:
+            if json_data["code"] == -1100:
+                await ctx.send("Invalid symbol. Format example: BTCUSDT")
+            if json_data["code"] == -1121:
+                await ctx.send("Pair does not exist.")
+        else:
+            price_change = float(json_data["priceChange"])
+            if price_change > 0:
+                await ctx.send(
+                    f"{symbol} price pumped by: {price_change} and {json_data['priceChangePercent']}%"
+                )
+            elif price_change < 0:
+                await ctx.send(
+                    f"{symbol} price dumped by: {price_change} and {json_data['priceChangePercent']}%"
+                )
+            else:
+                await ctx.send(
+                    f"{symbol} price did not change in 24hrs. Use !price to find out current price."
+                )
 
 
 class BinanceAPI:
@@ -48,6 +74,10 @@ class BinanceAPI:
     def get_price(self, symbol):
         payload = {"symbol": symbol}
         return requests.get(self.BASE_URL + "v3/ticker/price", params=payload)
+
+    def get_24hrdata(self, symbol):
+        payload = {"symbol": symbol}
+        return requests.get(self.BASE_URL + "v3/ticker/24hr", params=payload)
 
 
 if __name__ == "__main__":
