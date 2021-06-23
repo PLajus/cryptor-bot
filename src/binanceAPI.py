eeab6d1a85a37aaf5511429e1a8c242664ba9b07
Povilas Lajus <povilas.lajus@gmail.com>
2021-06-23 20:53:54 +0300

    Removed print statements

--- src/binanceAPI.py
+++ src/binanceAPI.py
@@ -2,26 +2,23 @@
 
 import requests
 
+
 # Exception handling for HTTP GET requests
 def exception_handler_get(func):
     def wrapper(*args, **kwargs):
         try:
-            r = func(*args, **kwargs)
-            r.raise_for_status()
+            resp = func(*args, **kwargs)
+            resp.raise_for_status()
         except requests.exceptions.HTTPError as http_err:
-            print(f"HTTP Error: {http_err}")
             raise http_err
         except requests.exceptions.ConnectionError as errc:
-            print(f"Error Connecting: {errc}")
             raise errc
         except requests.exceptions.Timeout as errt:
-            print(f"Timeout Error: {errt}")
             raise errt
         except Exception as err:
-            print(f"Something went wrong: {err}")
             raise err
         else:
-            return func(*args, **kwargs)
+            return resp
 
     return wrapper
 
